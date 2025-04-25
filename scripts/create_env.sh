30b9b173406b84ca3955581107ba646d614fdba9
AnkithRB <ankithrbelavadigmail.com>
2025-04-25 14:33:10 +0530

    all

--- scripts/create_env.sh
+++ scripts/create_env.sh
@@ -22,7 +22,9 @@ echo "Installing Python and pip..."
 if ! command -v python3 &> /dev/null
 then
     apt update
-    apt install -y python3 python3-pip python3-venv
+    apt install -y python3
+    apt install -y python3-pip
+    apt install -y python3-venv
 fi
 
 # Install virtualenv (if not installed)
